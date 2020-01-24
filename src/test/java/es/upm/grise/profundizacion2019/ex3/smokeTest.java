ff14d775db1d09173d6b88116df03dd7cf403c7d
fernando <fernando@fernando.com>
2020-01-24 15:37:25 +0100

    Add state test

--- src/test/java/es/upm/grise/profundizacion2019/ex3/smokeTest.java
+++ src/test/java/es/upm/grise/profundizacion2019/ex3/smokeTest.java
@@ -2,6 +2,9 @@ package es.upm.grise.profundizacion2019.ex3;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -17,5 +20,16 @@ public class smokeTest {
 	public void smoleTest() {
 		long SECONDS = 60;
 		my.nextTime(SECONDS);
+	}
+	
+	@Test
+	public void stateTestTimeClass ()
+	{
+		LocalDateTime actualDt = LocalDateTime.of(2020, 1, 1, 0, 0, 0);
+		LocalDateTime futureDt = LocalDateTime.of(2020, 1, 1, 0, 2, 0);
+		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+		
+		assertEquals(futureDt.format(formatter), Time.getFutureTime(actualDt, 120));
 	}
 }
