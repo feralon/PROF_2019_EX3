a62bb233c80f21c5c99612f491d958f46f193bc7
fernando <fernando@fernando.com>
2020-01-24 15:23:05 +0100

    Cambio 1: Crear param time en getFuture time que indica el tiempo actual

--- src/main/java/es/upm/grise/profundizacion2019/ex3/MyClass.java
+++ src/main/java/es/upm/grise/profundizacion2019/ex3/MyClass.java
@@ -1,9 +1,11 @@
 package es.upm.grise.profundizacion2019.ex3;
 
+import java.time.LocalDateTime;
+
 public class MyClass {
 	
 	public void nextTime(long seconds) {
-		String next = Time.getFutureTime(seconds);
+		String next = Time.getFutureTime(LocalDateTime.now(),seconds);
 		System.out.println(next);
 	}
 
--- src/main/java/es/upm/grise/profundizacion2019/ex3/Time.java
+++ src/main/java/es/upm/grise/profundizacion2019/ex3/Time.java
@@ -4,10 +4,14 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Time {
-	
-	public static String getFutureTime(long seconds) {
+	/**
+	 * 
+	 * @param time Datetime from we are getting the future time
+	 * @param seconds to forward
+	 * @return final Datetime
+	 */
+	public static String getFutureTime(LocalDateTime time, long seconds) {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
-		LocalDateTime time = LocalDateTime.now();
 		time.plusSeconds(seconds);
 		return formatter.format(time);	
 	}
